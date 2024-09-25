5e5c7f0edc38edc42f8474555527f7c1146b894d
An <trinhhuuan372@gmail.com>
2024-09-26 03:44:54 +0700

    Add FirstDialog.java

--- TrinhHuuAn_20225593_Lab1/src/HelloWorld.java
+++ TrinhHuuAn_20225593_Lab1/src/HelloWorld.java
@@ -1,6 +1,11 @@
+//Example 1: HelloWorld.java
+//Text printing program
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("TrinhHuuAn_20225593");
-        System.out.println("HelloWorld");
-    }
+        System.out.println("Trinh Huu An - 20225593");
+        System.out.println("Xin chao \n cac ban");
+        System.out.println("Hello \t world!");
+    } //end of method name
+
 }

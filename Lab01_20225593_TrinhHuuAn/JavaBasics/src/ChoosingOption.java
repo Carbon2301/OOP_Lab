ffea4a7717734e3738671accdb388e82b3f088e1
Carbon2301 <trinhhuuan372@gmail.com>
2024-09-30 18:05:32 +0700

    Update ChoosingOption

--- Lab01_20225593_TrinhHuuAn/JavaBasics/src/ChoosingOption.java
+++ Lab01_20225593_TrinhHuuAn/JavaBasics/src/ChoosingOption.java
@@ -6,6 +6,7 @@ public class ChoosingOption {
         int option = JOptionPane.showConfirmDialog(null,
                 "(AnTH - 20225593)Do you want to change the first class ticket?");
 
+
         // Hiển thị một thông báo dựa trên kết quả lựa chọn của người dùng (Yes hoặc No)
         JOptionPane.showMessageDialog(null, "You have chosen: " +
                 (option == JOptionPane.YES_OPTION ? "Yes" : "No"));
@@ -15,3 +16,29 @@ public class ChoosingOption {
         System.exit(0);
     }
 }
+
+//Cải tiến chương trình Choosing Option:
+//import javax.swing.JOptionPane;
+//
+//public class ChoosingOption {
+//    public static void main(String[] args) {
+//        // Định nghĩa các tùy chọn tùy chỉnh
+//        Object[] options = {"I do", "I don't"};
+//
+//        // Hiển thị hộp thoại tùy chỉnh với các tùy chọn đã định nghĩa
+//        int option = JOptionPane.showOptionDialog(null,
+//                "(AnTH - 20225593) Do you want to change the first class ticket?",
+//                "Chosen option",  // Tiêu đề của hộp thoại
+//                JOptionPane.YES_NO_OPTION,  // Loại hộp thoại tùy chọn
+//                JOptionPane.QUESTION_MESSAGE,  // Loại thông báo
+//                null,  // Không có biểu tượng tùy chỉnh
+//                options,  // Các nút tùy chỉnh
+//                options[0]);  // Tùy chọn mặc định được chọn
+//
+//        // Hiển thị tùy chọn mà người dùng đã chọn
+//        JOptionPane.showMessageDialog(null, "You have chosen: " +
+//                (option == JOptionPane.YES_OPTION ? "I do" : "I don't"));
+//
+//        System.exit(0);
+//    }
+//}

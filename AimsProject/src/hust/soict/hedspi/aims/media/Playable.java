f26dc188fc7381157d6ce5996f23c3d9d089ed86
Hữu An <trinhhuuan372@gmail.com>
2024-12-03 00:18:43 +0700

    Completed topic/aims-project/add-media-class

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Playable.java
@@ -0,0 +1,5 @@
+package hust.soict.hedspi.aims.media;
+// Trinh Huu An 20225593
+public interface Playable {
+    public void play();
+}
---
+++ AimsProject/src/hust/soict/hedspi/test/cart/CartTest.java
@@ -0,0 +1,37 @@
+package hust.soict.hedspi.test.cart;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.Media;
+
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        Media dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        cart.addMedia(dvd1);
+
+        Media dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addMedia(dvd2);
+//
+//        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+//                "Animation", 18.99f);
+//        cart.addDigitalVideoDisc(dvd3);
+
+        cart.printCart();
+
+//        System.out.println("Enter the title of DVD you want to search: ");
+//        Scanner sc = new Scanner(System.in);
+//        int idForSearch = sc.nextInt();
+//        Scanner sc = new Scanner(System.in);
+//        String titleForSearch = sc.nextLine();
+//
+//        if(cart.search(titleForSearch))
+//            System.out.println("Found DVD with title " + titleForSearch + " and the id of this DVD is " + cart.getDiscId(titleForSearch));
+//        else
+//            System.out.println("Not found DVD with title " + titleForSearch);
+
+    }
+}
